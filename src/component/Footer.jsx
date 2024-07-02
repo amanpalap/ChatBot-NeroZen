ba1d0947aa7586b5fa0fcf4e48fcda645bdd90c5
amanpalap <pal90589500@gmail.com>
2024-07-03 00:34:39 +0530

    update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bottom-0 flex flex-wrap justify-end w-full p-4 text-sm font-thin text-left text-white bg-black'>
+    <div className='bottom-0 flex flex-wrap justify-start w-full p-4 text-sm font-thin text-left text-white bg-black'>
       <p className='flex'>Made by team <h3 className='px-1 font-bold text-red-500'>"Machine Mavericks"</h3></p>
     </div>
   )
